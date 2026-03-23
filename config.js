c46b255923915314b12842d5353ab944b0f8f5ea
piyawat043 <126621386+piyawat043@users.noreply.github.com>
2026-03-24 04:39:11 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const buttonConfig = {
+    xyz: {
+        id: "xyz",
+        defaultText: "XYZ SENSOR (3 STEPS)",
+        videos: ['vdo_cnc/xyz01.mp4', 'vdo_cnc/xyz02.mp4', 'vdo_cnc/xyz03.mp4']
+    },
+    tool: {
+        id: "tool",
+        defaultText: "TOOL POSITION",
+        videos: ['vdo_cnc/toolposition.mp4']
+    },
+    cam: {
+        id: "cam",
+        defaultText: "CAMERA SETUP",
+        videos: ['vdo_cnc/camsetall.mp4']
+    }
+};
